5994fd257407c9a199aa81246bc9db5173935f14
Hadar-Cohen <hadar5z5z5@gmail.com>
2021-06-13 10:23:28 +0300

    HC10:23
    hi

---
+++ finalServerSide/finalServerSide/Scripts/navbarFunc.js
@@ -0,0 +1,21 @@
+﻿function navBarVisability() {
+    //localStorage
+    if (localStorage.user != null) {
+        user = JSON.parse(localStorage["user"]);
+        //$("#userName").text("Hello " + user.FirstName + " " + user.LastName);
+        document.getElementById("signUp").style.visibility = "hidden";
+        document.getElementById("login").style.visibility = "hidden";
+        document.getElementById("exit").style.visibility = "visibility";
+        document.getElementById("admin").style.visibility = "hidden";
+        if (user.FirstName == "Administrator") {
+            //document.getElementById("admin").style.visibility = "visibile";
+            $("#admin").prop("visibility", "visible");
+        }
+    }
+    else {
+        document.getElementById("signUp").style.visibility = "visibility";
+        document.getElementById("login").style.visibility = "visibility";
+        document.getElementById("exit").style.visibility = "hidden";
+        document.getElementById("admin").style.visibility = "hidden";
+    }
+}
